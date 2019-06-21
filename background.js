2bcbbc38bd4526d109459f4796bf83de2ba32f8c
jwkim960315 <jkim11@wesleyan.edu>
2019-06-21 22:38:14 +0900

    fixed background.js bug

--- background.js
+++ background.js
@@ -1,12 +1,20 @@
-chrome.tabs.query({ active: true },tab => {
-    console.log(tab);
-    chrome.contextMenus.create({
-        title: 'Open in new tab',
-        contexts: ["browser_action"],
-        id: `${tab[0].id}`
-    });
+// chrome.tabs.query({ active: true },tab => {
+//     console.log(tab);
+//     chrome.contextMenus.create({
+//         title: 'Open in new tab',
+//         contexts: ["browser_action"],
+//         id: `${tab[0].id}`
+//     });
+//
+//     chrome.contextMenus.onClicked.addListener((obj,tab) => {
+//         chrome.tabs.create({ url: 'options.html' });
+//     });
+// });
 
-    chrome.contextMenus.onClicked.addListener((obj,tab) => {
+chrome.contextMenus.create({
+    title: 'Open in new tab',
+    contexts: ["browser_action"],
+    onclick: clickedData => {
         chrome.tabs.create({ url: 'options.html' });
-    });
+    }
 });
