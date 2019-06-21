5795a958069565ed621c1a82b8c59583ce37a0ec
jwkim960315 <jkim11@wesleyan.edu>
2019-06-21 14:19:22 +0900

    can access options page via context menu

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.tabs.query({ active: true },tab => {
+    console.log(tab);
+    chrome.contextMenus.create({
+        title: 'Open in new tab',
+        contexts: ["browser_action"],
+        id: `${tab[0].id}`
+    });
+
+    chrome.contextMenus.onClicked.addListener((obj,tab) => {
+        chrome.tabs.create({ url: 'options.html' });
+    });
+});
