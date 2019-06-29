2aad8035d6fae403ac54a4705df8fc5d98a1ef40
jwkim960315 <jkim11@wesleyan.edu>
2019-06-29 10:18:12 +0900

    re-designing popup.html

--- popupRenderers.js
+++ popupRenderers.js
@@ -2,8 +2,8 @@ const renderNewGroupInput = target => {
     $(target).after(`
         <div class="row new-group-input">
             <div class="input-field col s12">
-                <label for="new-group" class="active white-text">Group Name</label>
-                <input id="new-group" type="text" name="groupName" class="white-text" autofocus/>
+                <label for="new-group" class="active">Group Name</label>
+                <input id="new-group" type="text" name="groupName" class="" autofocus/>
                 <span class="helper-text"></span>
             </div>
         </div>
